89940daced30e718bd16f9227efd9e56d218ad56
Andrey-Alohin <andriealohin2020@outlook.com>
2025-07-25 08:19:52 +0300

    refactor + add model|router|controller for auth

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import { getEnvVar } from './utils/getEnvVar.js';
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
-import contactsRouter from './routers/contacts.js';
+import router from './routers/index.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
@@ -22,7 +22,7 @@ export const startServer = () => {
     }),
   );
 
-  app.use('/contacts', contactsRouter);
+  app.use(router);
 
   app.use(notFoundHandler);
 
